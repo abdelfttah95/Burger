2cf0d12b816c2cf1f67f5dbe12fc19845e1d4d5c
abdelfttah95 <abdelfttah.sameeh@gmail.com>
2021-10-09 15:06:41 +0200

    first commit

---
+++ src/burger/DeluxeBurger.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package burger;
+
+/**
+ *
+ * @author abd elfttah
+ */
+public class DeluxeBurger extends Hamburger {
+    public DeluxeBurger() {
+        super("Deluxe", "Sausage & Bacon", 14.54, "White");
+        super.addHamburgerAddition1("Chips", 2.75);
+        super.addHamburgerAddition2("Drink", 1.81);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+}
